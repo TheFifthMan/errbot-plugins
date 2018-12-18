548fec1d1eba0cbdb809cec50e38b63e515b71e4
John Wen <John.Wen@ehealth.com>
2018-12-18 19:36:28 +0800

    .

--- plugins/gank/gank.py
+++ plugins/gank/gank.py
@@ -1,6 +1,6 @@
 # coding: utf-8 
 from errbot import BotPlugin, botcmd, arg_botcmd, webhook
-
+import requests
 
 class Gank(BotPlugin):
     """
@@ -9,5 +9,25 @@ class Gank(BotPlugin):
 
     @botcmd
     def gank_help(self,msg,args):
-        content = "```\n# get today news \n!gank today\n# get 福利 \n !gank --type=fuli --count=10 --page=1\n```".encode('utf-8').decode('utf-8')
+        content = "```\n# get today news \n!gank today\n# get 福利 \n !gank --type=fuli --count=10 --page=1\n```"
         return content
+    @botcmd
+    def gank_today(self,msg,args):
+        content = ""
+        url = "http://gank.io/api/today"
+        r = requests.get(url,verify=False)
+        res = r.json()
+        categories = res['category']
+        results = res['results']
+        for category in categories:
+            for result in results[category]:
+                des = result['desc']
+                res_url = result['url']
+                content += "```"+des+"\n"+"refer: "+res_url+"```\n"
+        
+        return content
+    
+    @arg_botcmd('--type',dest="set type for what your want",type=str)
+    @arg_botcmd('--count', dest='the number of result', type=int, default=5)
+    def gank(self,msg,args):
+        return type
--- plugins/gank/test_gank.py
+++ plugins/gank/test_gank.py
@@ -2,5 +2,5 @@ pytest_plugins = ["errbot.backends.test"]
 extra_plugin_dir = '.'
 
 def test_command(testbot):
-    testbot.push_message('!gank help')
-    assert "Hello" in testbot.pop_message()
+    testbot.push_message('!gank --type="fuli" --count=10')
+    assert "refer" in testbot.pop_message()
